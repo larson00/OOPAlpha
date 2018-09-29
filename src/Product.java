df5c779391ebcef63193ac13d05881bb01894c20
larson00 <larson05@gmail.com>
2018-09-29 12:59:47 -0400

    Step 4 complete

--- src/Product.java
+++ src/Product.java
@@ -8,10 +8,10 @@ public abstract class Product implements Item {
 // String manufacturer
 // Date manufacuredOn
 // String name
-  int serialNumber;
-  String manufacturer;
-  Date manufacuredOn;
-  String name;
+  private int serialNumber;
+  private String manufacturer;
+  private Date manufacuredOn;
+  private String name;
   // Add an integer class variable called currentProductionNumber.
 // This will store the next number to be assigned to serialNumber.
   static int currentProductionNumber;
@@ -36,27 +36,29 @@ public abstract class Product implements Item {
   public int getSerialNumber() {
     return serialNumber;
   }
-//  Add a constructor that will take in the name of the product and set this to the field variable
+
+  //  Add a constructor that will take in the name of the product and set this to the field variable
 // name.
 // You will also assign a serial number from the currentProductionNumber.
 // The currentProductionNumber should be incremented in readiness for the next instance.
 // Set manufacturedOn as the current date and time
-  public Product(String name){
+  public Product(String name) {
     this.name = name;
     serialNumber = currentProductionNumber;
     currentProductionNumber++;
     manufacuredOn = new Date(System.currentTimeMillis());
   }
-//  Add a toString method that will return the following: (example data shown).
+
+  //  Add a toString method that will return the following: (example data shown).
 //  Manufacturer : OraclProduction
 //  Serial Number : 1
 //  Date : Thu May 14 15:18:43 BST 2015
 //  Name : Product Name
-  public String toString(){
-    return "Manufacturer: " + manufacturer+ "\n" +
-    "Serial Number: " + serialNumber + "\n" +
-    "Date: " + manufacuredOn + "\n" +
-    "Name :" + name + "\n";
+  public String toString() {
+    return "Manufacturer: " + manufacturer + "\n" +
+        "Serial Number: " + serialNumber + "\n" +
+        "Date: " + manufacuredOn + "\n" +
+        "Name :" + name + "\n";
   }
 
 }
